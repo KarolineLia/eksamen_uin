702118edb8a708245769b3330c58e80b04746896
KarolineLia <karoliml@hiof.no>
2023-05-18 21:01:23 +0200

    tester om ting fungerer

--- eksamen_lia/src/components/GameShop.js
+++ eksamen_lia/src/components/GameShop.js
@@ -6,7 +6,7 @@ export default function GameShop({newgames}) {
             <section>
             {newgames?.slice(0,10).map((games, index) => (
                 <GameCard key={index} img={games?.background_image} title={games?.name} genre={games?.genres} slug={games?.slug}/>
-            ))}  
+            ))}
             </section>
         </div>
     )
