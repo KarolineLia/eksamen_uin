622918d62226b3d18c6eb27b063a4430c92e0e3d
KarolineLia <karoliml@hiof.no>
2023-05-19 03:02:52 +0200

    fikset knapper n stuff

--- eksamen_lia/src/components/GamePage.js
+++ eksamen_lia/src/components/GamePage.js
@@ -1,6 +1,24 @@
-export default function GamePage({title}) {
+import { useParams } from "react-router-dom"
 
-    return(
-        <h1>{title}</h1>
-    )
+export default function GamePage({newgames, genre}) {
+    const {slug} = useParams()
+    const gameshop = newgames.find((rec) => rec?.slug === slug)
+
+    console.log(gameshop)
+    if(slug === gameshop.slug) {
+        return(
+            <div className="gamePage">
+                <img src={gameshop.background_image}/>
+                <h1>{gameshop.name}</h1>
+                <p>Genre: {gameshop?.genres?.map(genre => genre.name).join(', ')}</p>
+                <p>Published: {gameshop.released}</p>
+                <p>Tags: {gameshop?.tags?.slice(0,1).map(tag => tag.name)}</p>
+
+                <button id="metacritic" onClick={() => (window.location.href = `https://www.metacritic.com/game/pc/${slug}`)}>{gameshop.metacritic}</button>
+
+                <button onClick={() => (window.location.href = `https://rawg.io/games/${slug}`)}>Buy</button>
+            </div>
+        )
+    }
+
 }
